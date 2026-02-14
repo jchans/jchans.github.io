24c2e3311eec47b95222826496710ba01afd8c97
jchans <jiangchenghans@gmail.com>
2026-02-14 15:01:28 +0800

    add notes

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,5 +3,6 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 
+  eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("bundle.css");
 }
