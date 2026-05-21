7fd63a06afa3488f546ea5feb19fbbcb76da8fc0
jchans <jiangchenghans@gmail.com>
2026-05-21 18:22:21 +0800

    add notes

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,5 +4,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 
   eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("favicon.svg");
+  eleventyConfig.addPassthroughCopy("favicon.png");
   eleventyConfig.addPassthroughCopy("bundle.css");
 }
---
+++ trpg/index.md
@@ -0,0 +1,7 @@
+---
+title: TRPG
+date: 2026-05-21
+show: false
+---
+
+- [【 TRPG問卷集 】01｜安全工具「X卡」](./surveys/survey-01-xcard)
